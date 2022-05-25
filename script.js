46f24239b276423babaf790fce8877a8abf6ddf8
isaiah <isaiah.keller@gmail.com>
2022-05-25 16:20:47 -0700

    added README, found bug where the Tab, Quote, and Slash keys all stayed pressed because the cause a browser event like tabbing out of the current window.  Fixed with e.preventDefault()

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ document.addEventListener('keydown', addPressed);
 document.addEventListener('keyup', removePressed);
 
 function addPressed(e) {
+    e.preventDefault()
     console.log(`${e.code}`)
     document.getElementById(`${e.code}`).classList.add("pressed");
 }
 
-// function removePressed(e) {
-//     console.log(`${e.code}`)
-//     document.getElementById(`${e.code}`).classList.remove("pressed");
-// }
+function removePressed(e) {
+    console.log(`${e.code}`)
+    document.getElementById(`${e.code}`).classList.remove("pressed");
+}
