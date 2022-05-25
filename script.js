c025bdab735a60bbf070d38015e15e9a6e2c712c
isaiah <isaiah.keller@gmail.com>
2022-05-25 16:00:10 -0700

    created keydown and keyup events that successfully add and remove the pressed class

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 
-document.addEventListener('keydown', logKey);
 
-function logKey(e) {
-    console.log(` ${e.code}`)
-  }
+document.addEventListener('keydown', addPressed);
+document.addEventListener('keyup', removePressed);
+
+function addPressed(e) {
+    console.log(`${e.code}`)
+    document.getElementById(`${e.code}`).classList.add("pressed");
+}
+
+// function removePressed(e) {
+//     console.log(`${e.code}`)
+//     document.getElementById(`${e.code}`).classList.remove("pressed");
+// }
