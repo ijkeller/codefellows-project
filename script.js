057908590b8171ea6557923bb8d2e849b0850fc0
isaiah <isaiah.keller@gmail.com>
2022-05-26 10:41:45 -0700

    tab bug fix found on stackoverflow

--- script.js
+++ script.js
@@ -20,3 +20,39 @@ function addPressed(e) {
 function removePressed(e) {
     document.getElementById(`${e.code}`).classList.remove("pressed");
 }
+
+// ---------------------------------------
+
+// *** This code coppied from stackoverflow to fix tab bug ***
+// https://stackoverflow.com/questions/6140632/how-to-handle-tab-in-textarea
+
+// ---------------------------------------
+
+document.querySelector("textarea").addEventListener('keydown',function(e) {
+    if(e.keyCode === 9) { // tab was pressed
+        // get caret position/selection
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        var target = e.target;
+        var value = target.value;
+
+        // set textarea value to: text before caret + tab + text after caret
+        target.value = value.substring(0, start)
+                    + "\t"
+                    + value.substring(end);
+
+        // put caret at right position again (add one for the tab)
+        this.selectionStart = this.selectionEnd = start + 1;
+
+        // prevent the focus lose
+        e.preventDefault();
+    }
+},false);
+
+// ---------------------------------------
+
+// *** End copied code ***
+
+// ---------------------------------------
+
